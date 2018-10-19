b4c954e0c6993def5917ce5873efa2ee0373fd16
mssigler <mssigler@gmail.com>
2018-10-19 03:36:26 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,41 @@
 //define functions here
 
+function getIt(){
+  $("p").on("click", function(){
+    alert("Hey!")
+  } )
+}
+
+
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  } )
+}
+
+function pressIt(){
+  $('document').on('keydown', function(){  // input ok?
+      // below, tried 'input'    'event.key'
+      if (('keydown').which == 71){    //changed fm 'input'
+        alert('you pressed G.');
+        
+      }
+    }
+)}
+
+
+
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
+
 $(document).ready(function(){
-
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt();
 });
